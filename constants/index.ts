9114993d47067b6a8a5cb19c2e4067bc67caf917
Bogdan <bogdan.pavliv@gmail.com>
2024-11-20 16:35:02 +0200

    add project

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { FaYoutube, FaFacebook, FaInstagram } from "react-icons/fa";
+
+export const navLinks = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Treatments", href: "/treatments" },
+  { name: "Contact", href: "/contact" },
+];
+
+export const socialLinks = [
+  { name: "Youtube", href: "https://www.youtube.com", icon: FaYoutube },
+  { name: "Facebook", href: "https://www.facebook.com", icon: FaFacebook },
+  { name: "Twitter", href: "https://x.com", icon: FaXTwitter },
+  { name: "Instagram", href: "https://www.instagram.com", icon: FaInstagram },
+];
+
+export const statList = [
+  { countNum: 13, countText: "", text: "Years on Market" },
+  { countNum: 35, countText: "k+", text: "Happy Clients" },
+  { countNum: 97, countText: "%", text: "Natural Ingredients" },
+];
+
+export const animations = {
+  initial: { top: "0%" },
+  animate: { top: "-100%" },
+  exit: { top: ["100%", "0%"] },
+};
